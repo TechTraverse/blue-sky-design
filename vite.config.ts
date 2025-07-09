04b7f1f07a2df28c50e7b35964fb755e2a5c7e08
Aaron Caldwell <Aaron.Caldwell@noaa.gov>
2025-07-09 10:50:10 -0600

    Transition to lib packager

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ tsconfigPath: './tsconfig.lib.json' })
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+    }
+  }
 })
