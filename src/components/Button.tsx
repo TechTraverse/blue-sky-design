d5e67f9e4ea1f40499d39501c9723a27e4de2dfd
Aaron Caldwell <Aaron.Caldwell@noaa.gov>
2025-07-11 14:35:16 -0600

    Update button template to make usable for other components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,14 @@
-// import React from 'react';
-
 import './button.css';
 
 export interface ButtonProps {
   /** Is this the principal call to action on the page? */
   primary?: boolean;
-  /** What background color to use */
-  backgroundColor?: string;
+  /** What classes to apply */
+  className?: string;
   /** How large should the button be? */
   size?: 'small' | 'medium' | 'large';
   /** Button contents */
-  label: string;
+  children?: React.ReactNode;
   /** Optional click handler */
   onClick?: () => void;
 }
@@ -19,19 +17,18 @@ export interface ButtonProps {
 export const Button = ({
   primary = false,
   size = 'medium',
-  backgroundColor,
-  label,
+  className = '',
+  children = <>{'Replace me'}</>,
   ...props
 }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
+      className={['storybook-button', `storybook-button--${size}`, className, mode].join(' ')}
       {...props}
     >
-      {label}
+      {children}
     </button>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import react from '@vitejs/plugin-react-swc'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    dts({ tsconfigPath: './tsconfig.lib.json' })
+    dts({ tsconfigPath: './tsconfig.lib.json' }),
+    libInjectCss(),
   ],
   build: {
     copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, 'src/lib.ts'),
       formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react'],
     }
   }
 })
