a69ab4b5892bd629a545db546f5937a58b602afb
Aaron Caldwell <Aaron.Caldwell@noaa.gov>
2025-07-22 11:40:54 -0600

    Translate month to plain english

--- src/components/TimeRangeSlider/HorizontalCalendar.tsx
+++ src/components/TimeRangeSlider/HorizontalCalendar.tsx
@@ -4,7 +4,7 @@ import f from 'lodash/fp';
 import type { RangeValue } from "@react-types/shared";
 import { CalendarDate, getDayOfWeek as _getDayOfWeek, type DateValue } from "@internationalized/date";
 import { DateTime, Duration } from "effect";
-import { useCalendarGrid } from 'react-aria';
+import { useCalendarGrid, useLocale } from 'react-aria';
 import type { RangeCalendarState } from "react-stately";
 
 type DayData = Pick<CalendarDate, 'year' | 'month' | 'day'> & {
@@ -24,6 +24,24 @@ const getDayOfWeek = (date: CalendarDate): string => {
     default: return '';
   }
 };
+
+const getMonth = (number: number): string => {
+  switch (number) {
+    case 1: return 'January';
+    case 2: return 'February';
+    case 3: return 'March';
+    case 4: return 'April';
+    case 5: return 'May';
+    case 6: return 'June';
+    case 7: return 'July';
+    case 8: return 'August';
+    case 9: return 'September';
+    case 10: return 'October';
+    case 11: return 'November';
+    case 12: return 'December';
+    default: return '';
+  }
+}
 
 const getDayData = (date: CalendarDate): DayData => ({
   ...date,
@@ -51,6 +69,7 @@ export const HorizontalCalendar = ({ duration, viewStartDateTime }: {
   duration: Duration.Duration,
   viewStartDateTime: DateTime.DateTime
 }) => {
+
   // const calendarGrid = useCalendarGrid({}, state);
   const start = new CalendarDate(
     DateTime.getPart(viewStartDateTime, 'year'),
@@ -72,9 +91,11 @@ export const HorizontalCalendar = ({ duration, viewStartDateTime }: {
     <div className="horizontal-calendar-grid">
       {daysByMonth.map(x => {
         const { month, year } = x[0];
+        // Plain english month name
+        const plainEnglishMonth = getMonth(month);
 
         return (<div className="month-column">
-          <div className="month-header">{`${month}/${year}`}</div>
+          <div className="month-header">{`${plainEnglishMonth} ${year}`}</div>
           <table>
             <tbody className="horizontal-calendar-grid-body">
               <tr>
