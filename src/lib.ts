982b5ef643c8f8d0c2fffd6cc6d2ce5ad3b43ccd
Aaron Caldwell <Aaron.Caldwell@noaa.gov>
2025-08-27 15:28:29 -0600

    Export time slider and time duration enum

--- src/lib.ts
+++ src/lib.ts
@@ -5,3 +5,6 @@ export { Header } from './components/Header';
 export type { HeaderProps } from './components/Header';
 
 export { Page } from './components/Page';
+
+export { TimeRangeSlider } from './components/TimeRangeSlider/TimeRangeSlider';
+export { TimeDuration } from './components/TimeRangeSlider/timeSliderTypes';
