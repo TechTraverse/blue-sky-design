be1d0802e755787234f53bdf8d924c4b961589e2
Aaron Caldwell <Aaron.Caldwell@noaa.gov>
2026-03-09 21:54:20 -0600

    React type mismatch fix

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['react'],
+      external: ['react', 'react-dom', 'react/jsx-runtime', 'react/jsx-dev-runtime'],
     }
   }
 })
