d127e574d6b987d45895372b704bc0af5edee97f
Aaron Caldwell <Aaron.Caldwell@noaa.gov>
2026-03-30 10:05:28 -0600

    Add ScaledRangeSlider barrel exports

---
+++ src/components/ScaledRangeSlider/index.ts
@@ -0,0 +1,26 @@
+/**
+ * ScaledRangeSlider barrel exports
+ */
+
+export { ScaledRangeSlider, default } from './ScaledRangeSlider';
+export type {
+  ScaledRangeSliderProps,
+  ColorStop,
+  SliderMark,
+  ScalePreset,
+  ScaleFunction,
+  InverseScaleFunction,
+} from './types';
+export {
+  getScaleFunctions,
+  normalizeScaleConfig,
+  linearScale,
+  linearInverse,
+  cubicScale,
+  cubicInverse,
+  quadraticScale,
+  quadraticInverse,
+  logarithmicScale,
+  logarithmicInverse,
+} from './scaleUtils';
+export { colorStopsToGradient, colorStopsToMarks } from './colorUtils';
